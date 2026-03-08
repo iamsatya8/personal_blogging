5e56987dd4365f4160a2ef29de0d8e91c0dc26f9
iamsatya8 <vsaisatyanarayana@gmail.com>
2026-03-08 18:09:12 +0530

    Initial commit - Spring Boot project

---
+++ src/main/java/com/satya/projects/personal_blogging/PersonalBloggingApplication.java
@@ -0,0 +1,13 @@
+package com.satya.projects.personal_blogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBloggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satya/projects/personal_blogging/PersonalBloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satya.projects.personal_blogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
